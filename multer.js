5d8170eb7a06b4b6657d137214ac6bde64dc48cf
Aman Tiwari <114355313+coderX56@users.noreply.github.com>
2024-07-08 23:50:03 +0530

    Add files via upload

---
+++ multer.js
@@ -0,0 +1,20 @@
+/** @format */
+
+const multer = require("multer");
+const crypto = require("crypto");
+const path = require("path");
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "./public/images/uploads/");
+	},
+	filename: (req, file, cb) => {
+		crypto.randomBytes(12, (err, bytes) => {
+			const fn = bytes.toString("hex") + path.extname(file.originalname);
+			cb(null, fn);
+		});
+	},
+});
+
+const upload = multer({ storage: storage });
+module.exports = upload;
